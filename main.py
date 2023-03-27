f04e2515c9aa064513b4a0f9c809eb58e4fbad0e
RajnandiniIngale <ingalerajnandini22@gmail.com>
2023-03-27 19:05:22 +0530

    analysis files added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 # nltk.download('vader_lexicon')
 
-import nltk
+
 import ssl
 
 import mimetypes
@@ -19,6 +19,7 @@ def index():
 
 @app.route('/tours')
 def tours():
+    stars={}
 #
     # try:
     #      _create_unverified_https_context = ssl._create_unverified_context
@@ -50,27 +51,65 @@ def tours():
         print("Compound Score:", scores['compound'])
         print()
 
-        stars = 0
+        starsfoodkeys = 0
 
         if scores["compound"] < 0:
-            stars = 1
+            starsfoodkeys = 1
         else:
 
             if scores["compound"] == 0 and scores["neu"] == 1:
-                stars = 2
+                starsfoodkeys = 2
             else:
                 if scores["compound"] > 0 and scores["compound"] <= 0.4:
-                    stars = 3
+                    starsfoodkeys = 3
                 else:
                     if scores["compound"] > 0 and scores["compound"] <= 0.6:
-                        stars = 4
+                        starsfoodkeys = 4
                     else:
                         if scores["compound"] > 0.6:
-                            stars = 5;
+                            starsfoodkeys = 5;
 
-        print("stars  " + ((str))(stars))
+        print("stars  " + ((str))(starsfoodkeys))
+        stars["starsfoodkeys"]=(str)(starsfoodkeys)
 
-    return render_template("tours.html",ratingsf=stars)
+    file1.close()
+    file1 = open('location.txt', 'r', encoding="utf8")
+    Lines = file1.readlines()
+
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        review = line
+        scores = sia.polarity_scores(review)
+        print(review)
+        print("Negative Score:", scores['neg'])
+        print("Neutral Score:", scores['neu'])
+        print("Positive Score:", scores['pos'])
+        print("Compound Score:", scores['compound'])
+        print()
+
+        starslocationkeys = 0
+
+        if scores["compound"] < 0:
+            starslocationkeys = 1
+        else:
+
+            if scores["compound"] == 0 and scores["neu"] == 1:
+                starslocationkeys = 2
+            else:
+                if scores["compound"] > 0 and scores["compound"] <= 0.4:
+                    starslocationkeys = 3
+                else:
+                    if scores["compound"] > 0 and scores["compound"] <= 0.6:
+                        starslocationkeys = 4
+                    else:
+                        if scores["compound"] > 0.6:
+                            starslocationkeys = 5;
+
+        print("stars  " + ((str))(starslocationkeys))
+        stars["starslocationkeys"] = (str)(starslocationkeys)
+
+    return render_template("tours.html",ratingsk=stars)
 
 
 @app.route('/review')
