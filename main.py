2efaa51c9746b95ba62089e560bfd066a874b3d2
RajnandiniIngale <ingalerajnandini22@gmail.com>
2023-03-27 11:46:20 +0530

    analysis files added

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+# nltk.download('vader_lexicon')
+
+import nltk
+import ssl
 
 import mimetypes
 from flask import Flask, render_template, request
@@ -13,7 +19,58 @@ def index():
 
 @app.route('/tours')
 def tours():
-    return render_template("tours.html")
+#
+    # try:
+    #      _create_unverified_https_context = ssl._create_unverified_context
+    # except AttributeError:
+    #      pass
+    # else:
+    #      ssl._create_default_https_context = _create_unverified_https_context
+
+    # nltk.download()
+
+    # Initialize the sentiment analyzer
+    sia = SentimentIntensityAnalyzer()
+
+    # Define a list of reviews to analyze
+
+    # Using readlines()
+    file1 = open('food.txt', 'r')
+    Lines = file1.readlines()
+
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        review = line
+        scores = sia.polarity_scores(review)
+        print(review)
+        print("Negative Score:", scores['neg'])
+        print("Neutral Score:", scores['neu'])
+        print("Positive Score:", scores['pos'])
+        print("Compound Score:", scores['compound'])
+        print()
+
+        stars = 0
+
+        if scores["compound"] < 0:
+            stars = 1
+        else:
+
+            if scores["compound"] == 0 and scores["neu"] == 1:
+                stars = 2
+            else:
+                if scores["compound"] > 0 and scores["compound"] <= 0.4:
+                    stars = 3
+                else:
+                    if scores["compound"] > 0 and scores["compound"] <= 0.6:
+                        stars = 4
+                    else:
+                        if scores["compound"] > 0.6:
+                            stars = 5;
+
+        print("stars  " + ((str))(stars))
+
+    return render_template("tours.html",ratingsf=stars)
 
 
 @app.route('/review')
