659a4d6d9bcdfba121497db5b3fc6112c647cc94
Snehalkamble123 <124338046+Snehalkamble123@users.noreply.github.com>
2023-04-02 23:35:28 +0530

    Add files via upload

---
+++ sayaji_tr.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+#nltk.download('vader_lexicon')
+
+import pandas as pd     #create dataframe read csv file & save file in csv format
+
+#read csv file
+
+data = pd.read_csv('park.csv')
+
+print(data)
