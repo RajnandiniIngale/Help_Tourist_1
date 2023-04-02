e58af82f50a15c5b486a42ef3fe4931bec68e3a4
admin <snehalkamble1212@gmail.com>
2023-04-02 23:39:19 +0530

    preprocess1 added

--- sayaji_tr.py
+++ sayaji_tr.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 #nltk.download('vader_lexicon')
@@ -9,3 +10,16 @@ import pandas as pd     #create dataframe read csv file & save file in csv forma
 data = pd.read_csv('park.csv')
 
 print(data)
+=======
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+#nltk.download('vader_lexicon')
+
+import pandas as pd     #create dataframe read csv file & save file in csv format
+
+#read csv file
+
+data = pd.read_csv('park.csv')
+
+print(data)
+>>>>>>> 368445d (preprocess1 added)
