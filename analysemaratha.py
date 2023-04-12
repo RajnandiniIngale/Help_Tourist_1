0657e5e856c61e1f175d6765782212a7a958d0f3
Snehalkamble123 <124338046+Snehalkamble123@users.noreply.github.com>
2023-04-12 22:10:20 +0530

    Add files via upload

---
+++ analysemaratha.py
@@ -0,0 +1,160 @@
+
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+#nltk.download('vader_lexicon')
+
+import nltk
+import ssl
+#
+# try:
+#      _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#      pass
+# else:
+#      ssl._create_default_https_context = _create_unverified_https_context
+
+#nltk.download()
+
+# Initialize the sentiment analyzer
+sia = SentimentIntensityAnalyzer()
+
+# Define a list of reviews to analyze
+
+# Using readlines()
+file1 = open('food.txt', 'r',encoding="utf-8")
+Lines = file1.readlines()
+ffile = open("maratha.txt", "w", encoding="utf-8")
+
+count = 0
+sum=0
+# Strips the newline character
+for line in Lines:
+    review=line
+    scores = sia.polarity_scores(review)
+    sum=sum+scores["compound"]
+
+    stars=0
+
+sum=sum/10
+
+if sum<0:
+    stars=1
+else:
+
+    if sum<=0.2:
+     stars=2
+    else:
+        if sum<=0.4:
+         stars=3
+        else:
+            if sum<=0.6:
+                stars=4
+            else:
+                if sum>0.6:
+                     stars=5;
+
+
+
+
+ffile.writelines("stars_food :" +((str))(stars)+"\n")
+
+file1 = open('clean.txt', 'r', encoding="utf-8")
+Lines = file1.readlines()
+print(sum)
+
+count = 0
+sum = 0
+# Strips the newline character
+for line in Lines:
+    review = line
+    scores = sia.polarity_scores(review)
+    #print(scores["compound"])
+    sum = sum + scores["compound"]
+
+    stars = 0
+sum=sum/10
+
+if sum < 0:
+    stars = 1
+else:
+
+    if sum <= 0.2:
+        stars = 2
+    else:
+        if sum <= 0.4:
+            stars = 3
+        else:
+            if sum <= 0.6:
+                 stars = 4
+            else:
+                if sum > 0.6:
+                    stars = 5;
+
+ffile.writelines("stars_clean :" + ((str))(stars) + "\n")
+
+
+print(sum)
+file1 = open('service.txt', 'r', encoding="utf-8")
+Lines = file1.readlines()
+
+
+count = 0
+sum = 0
+# Strips the newline character
+for line in Lines:
+    review = line
+    scores = sia.polarity_scores(review)
+    sum = sum + scores["compound"]
+
+    stars = 0
+sum = sum / 10
+if sum < 0:
+    stars = 1
+else:
+
+    if sum <= 0.2:
+        stars = 2
+    else:
+        if sum <= 0.4:
+            stars = 3
+        else:
+            if sum <= 0.6:
+                stars = 4
+            else:
+                if sum > 0.6:
+                    stars = 5;
+
+ffile.writelines("stars_service :" + ((str))(stars) + "\n")
+
+file1 = open('location.txt', 'r', encoding="utf-8")
+Lines = file1.readlines()
+print(sum)
+
+count = 0
+sum = 0
+# Strips the newline character
+for line in Lines:
+    review = line
+    scores = sia.polarity_scores(review)
+    sum = sum + scores["compound"]
+
+sum = sum / 10
+stars = 0
+
+if sum < 0:
+    stars = 1
+else:
+
+    if sum <= 0.2:
+        stars = 2
+    else:
+        if sum <= 0.4:
+            stars = 3
+        else:
+            if sum <= 0.6:
+                stars = 4
+            else:
+                if sum > 0.6:
+                    stars = 5;
+print(sum)
+ffile.writelines("stars_location :" + ((str))(stars) + "\n")
