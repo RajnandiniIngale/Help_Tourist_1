2fb99fea01fdd17e665956f74fdc57d24ad092ab
snehal kamble <snehalkamble1212@gmail.com>
2023-05-11 11:02:11 -0500

    anayse.py,chingarianalyse.py,barbeque-analyse.py modified

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+import csv
+import os
+import nltk
+
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 # nltk.download('vader_lexicon')
@@ -195,39 +203,42 @@ def insert_keys_csv():
         file1.writelines(rev+"  ")
 
         file1.close()
+        os.system('python C:\\Users\\USER\\PycharmProjects\\Help_Tourist_1\\sayaji_preprocess.py')
     if name=='maratha':
         file1 = open('maratha.csv', 'a+')
         file1.writelines("\n")
         file1.writelines(rev+"  ")
 
         file1.close()
-
+    os.system('python C:\\Users\\USER\\PycharmProjects\\Help_Tourist_1\\maratha_preprocess.py')
     if name=='parakh':
         file1 = open('parakh.csv', 'a+')
         file1.writelines("\n")
         file1.writelines(rev+"  ")
 
         file1.close()
-
+    os.system('python C:\\Users\\USER\\PycharmProjects\\Help_Tourist_1\\parakh_preprocess.py')
     if name=='tandoor':
         file1 = open('Tandoor.csv', 'a+')
         file1.writelines("\n")
         file1.writelines(rev+"  ")
 
         file1.close()
+        os.system('python C:\\Users\\USER\\PycharmProjects\\Help_Tourist_1\\tandoor_preprocess.py')
     if name=='mahalaxmi':
         file1 = open('mahalaxmi.csv', 'a+')
         file1.writelines("\n")
         file1.writelines(rev+"  ")
 
         file1.close()
+        os.system('python C:\\Users\\USER\\PycharmProjects\\Help_Tourist_1\\mahalakshmi_preprocess.py')
     if name=='newpalace':
         file1 = open('newpalace.csv', 'a+')
         file1.writelines("\n")
         file1.writelines(rev+"  ")
 
         file1.close()
-
+        os.system('python C:\\Users\\USER\\PycharmProjects\\Help_Tourist_1\\newpalace_preprocess.py')
     if name=='chingari':
         file1 = open('chingari.csv', 'a+')
         file1.writelines("\n")
@@ -249,9 +260,12 @@ def insert_keys_csv():
         file1 = open('bandrafort.csv', 'a+')
         file1.writelines("\n")
         file1.writelines(rev+"  ")
+        file1.close()
 
 
-    return render_template("review.html")
+
+    return render_template("index.html")
+
 
 
 @app.route('/mumbai')
