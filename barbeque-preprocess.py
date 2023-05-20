ed2facca12cfd14c677418aecfb83285043c5d44
RajnandiniIngale <ingalerajnandini22@gmail.com>
2023-05-20 14:37:28 +0530

    ..

--- barbeque-preprocess.py
+++ barbeque-preprocess.py
@@ -95,3 +95,233 @@ for k in arr1:           #traversing through the arr of useful words
 
 
 
+file.close()
+file1.close()
+file2.close()
+file3.close()
+
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+#nltk.download('vader_lexicon')
+
+import nltk
+import ssl
+# try:
+#      _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#      pass
+# else:
+#      ssl._create_default_https_context = _create_unverified_https_context
+
+#nltk.download()
+import pandas as pd
+import numpy as np
+# Initialize the sentiment analyzer
+sia = SentimentIntensityAnalyzer()
+
+# Define a list of reviews to analyze
+
+# Using readlines()
+file1 = open('bbqfood.txt', 'r',encoding="utf-8")
+Lines = file1.readlines()
+ffile = open("barbeque.txt", "w", encoding="utf-8")
+reviewfile = open("barbeque-reviews.txt","w",encoding="utf-8")
+
+count = 0
+sum=0
+# Strips the newline character
+for line in Lines:
+    review=line
+    scores = sia.polarity_scores(review)
+    sum=sum+scores["compound"]
+
+    reviewfile.writelines(review.rstrip() + ":" + str(scores['compound']))
+    reviewfile.writelines("\n")
+    stars = 0
+
+
+sum=sum/10
+
+if sum < 0.3:
+    stars = 1
+else:
+
+    if sum <= 0.5:
+        stars = 2
+    else:
+        if sum <= 1.5:
+            stars = 3
+        else:
+            if sum <= 2.5:
+                stars = 4
+            else:
+                if sum > 2.5:
+                    stars = 5;
+
+
+
+
+ffile.writelines("barbeque_food:" +((str))(stars)+"\n")
+
+file1 = open('bbqmoney.txt', 'r', encoding="utf-8")
+Lines = file1.readlines()
+print(sum)
+
+count = 0
+sum = 0
+# Strips the newline character
+for line in Lines:
+    review = line
+    scores = sia.polarity_scores(review)
+    #print(scores["compound"])
+
+    sum = sum + scores["compound"]
+    reviewfile.writelines(review.rstrip() + ":" + str(scores['compound']))
+    reviewfile.writelines("\n")
+    stars = 0
+
+sum=sum/10
+
+if sum < 0.3:
+    stars = 1
+else:
+
+    if sum <= 0.5:
+        stars = 2
+    else:
+        if sum <= 1.5:
+            stars = 3
+        else:
+            if sum <= 2.5:
+                stars = 4
+            else:
+                if sum > 2.5:
+                    stars = 5;
+
+ffile.writelines("barbeque_money:" + ((str))(stars) + "\n")
+
+
+print(sum)
+file1 = open('bbqservice.txt', 'r', encoding="utf-8")
+Lines = file1.readlines()
+
+
+count = 0
+sum = 0
+# Strips the newline character
+for line in Lines:
+    review = line
+    scores = sia.polarity_scores(review)
+    sum = sum + scores["compound"]
+
+    reviewfile.writelines(review.rstrip() + ":" + str(scores['compound']))
+    reviewfile.writelines("\n")
+    stars=0
+stars = 0
+sum = sum / 10
+
+if sum < 0.3:
+    stars = 1
+else:
+
+    if sum <= 0.5:
+        stars = 2
+    else:
+        if sum <= 1.5:
+            stars = 3
+        else:
+            if sum <= 2.5:
+                stars = 4
+            else:
+                if sum > 2.5:
+                    stars = 5;
+
+ffile.writelines("barbeque_service:" + ((str))(stars) + "\n")
+
+file1 = open('bbqlocation.txt', 'r', encoding="utf-8")
+Lines = file1.readlines()
+print(sum)
+
+count = 0
+sum = 0
+# Strips the newline character
+for line in Lines:
+    review = line
+    scores = sia.polarity_scores(review)
+    sum = sum + scores["compound"]
+
+    reviewfile.writelines(review.rstrip() + ":" + str(scores['compound']))
+    reviewfile.writelines("\n")
+    stars=0
+sum = sum / 10
+stars = 0
+
+if sum < 0.3:
+    stars = 1
+else:
+
+    if sum <= 0.5:
+        stars = 2
+    else:
+        if sum <= 1.5:
+            stars = 3
+        else:
+            if sum <= 2.5:
+                stars = 4
+            else:
+                if sum > 2.5:
+                    stars = 5;
+print(sum)
+ffile.writelines("barbeque_location:" + ((str))(stars) + "\n")
+
+
+
+file1.close()
+reviewfile.close()
+
+
+import numpy as np
+import ast
+import csv
+#Open a CSV file for writing
+with open('data.csv', 'w', newline='') as csvfile:
+#   # Define the header row of the CSV file
+   fieldnames = ['Reviews', 'ratings']
+
+#   # Create a CSV writer object
+   writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+#   # Write the header row
+   writer.writeheader()
+
+file1 = open('barbeque-reviews.txt', 'r',encoding='UTF-8')
+
+Lines = file1.readlines()
+my_dict={}
+count = 0
+# Strips the newline character
+for line in Lines:
+    review=line
+    arr=review.split(":",1)
+    if len(arr) > 1:
+        key=arr[0].replace(' ] [','')
+        my_dict[key]=(arr[1].rstrip())
+    #print(review)
+
+keys = list(my_dict.keys())
+values = list(my_dict.values())
+
+
+sorted_value_index = np.argsort(values)[::-1]    #sort in descending order ==> list of values
+sorted_dict = {keys[i]: values[i] for i in sorted_value_index}      # create dictionary from list
+
+print(sorted_dict)
+with open('static/js/bbqtest.csv', 'w',encoding='UTF-8') as f:
+    for key in sorted_dict.keys():
+        f.write("%s,%s\n"%(key,sorted_dict[key]))
+
+
+
+
+
+
